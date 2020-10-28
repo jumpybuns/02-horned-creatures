e0e391d3ba98b5711ca1615a1f1973b53451e248
jumpybuns <ethanpiercepresents@gmail.com>
2020-10-27 18:05:55 -0700

    yoyoyoyoy

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import './App.css';
 import React from 'react';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
+import images from './data.js';
 
 export default class App extends React.Component {
   render() {
       return (
           <div>
               <Header /> 
-              <ImageList />
-              <h1>The World of Horned Creatures</h1>
-              <option className="keyword">Keywords</option>
+              <ImageList imagesProps={images}/>
           </div>
       );
   }
 }
 
-
-// const numbers = [1, 2, 3, 4, 5,];
-// const keywords = numbers.filter(number => number === true);
--- src/Header.js
+++ src/Header.js
@@ -3,55 +3,10 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (
-            <div>
+            <div className="heads">
                 <h1>This is some horned animals right here</h1>
-                <Dropdown />
             </div>
         )
     }
 }
 
-export class Dropdown extends React.Component {
-
-        constructor() {
-          super();
-          
-          this.state = {
-            showMenu: false,
-          }
-          
-          this.showMenu = this.showMenu.bind(this);
-        }
-        
-        showMenu(event) {
-          event.preventDefault();
-          
-          this.setState({
-            showMenu: true,
-          });
-        }
-      
-        render() {
-          return (
-            <div>
-              <button onClick={this.showMenu}>
-                Show menu
-              </button>
-              
-              {
-                this.state.showMenu
-                  ? (
-                    <div className="menu">
-                      <button> Menu item 1 </button>
-                      <button> Menu item 2 </button>
-                      <button> Menu item 3 </button>
-                    </div>
-                  )
-                  : (
-                    null
-                  )
-              }
-            </div>
-          );
-        }
-      }
