1e612658704ae159b54b33caefc1e8f53accedb1
jumpybuns <ethanpiercepresents@gmail.com>
2020-10-28 08:33:48 -0700

    stuff

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,8 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className="heads">
-                <h1>This is some horned animals right here</h1>
+                <h1>This is Some Horned Animals Right Here</h1>
+                <p className="sortby">Sort By Horns:</p>
             </div>
         )
     }
